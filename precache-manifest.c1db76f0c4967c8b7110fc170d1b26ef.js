97c69a9ea471c4676fa662b61b257346f8981dcd
Jeremiah Bautista <jeremiah.bautista@gmail.com>
2020-09-16 12:54:39 -0700

    Updates

---
+++ precache-manifest.c1db76f0c4967c8b7110fc170d1b26ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bc1027be846c83177b2cf7bee747a7b",
+    "url": "/pics-subreddit/index.html"
+  },
+  {
+    "revision": "78eb840edf618895719b",
+    "url": "/pics-subreddit/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b1ab9f83ba670ea4a0b6",
+    "url": "/pics-subreddit/static/js/2.bc85caa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pics-subreddit/static/js/2.bc85caa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78eb840edf618895719b",
+    "url": "/pics-subreddit/static/js/main.ef693b1b.chunk.js"
+  },
+  {
+    "revision": "fa0431c061cad8c26fde",
+    "url": "/pics-subreddit/static/js/runtime-main.7b26a0bb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pics-subreddit/static/media/logo.5d5d9eef.svg"
+  }
+]);
