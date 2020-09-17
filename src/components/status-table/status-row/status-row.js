f6f73b25604cac3a36afc9318f12778118201973
Jeremiah Bautista <jeremiah.bautista@gmail.com>
2020-09-16 17:34:54 -0700

    feat(ui): formats time in table

--- src/components/status-table/status-row/status-row.js
+++ src/components/status-table/status-row/status-row.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import moment from 'moment';
 
 const StatusRow = ({ api, status }) => {
   const success = status?.success || false;
   const message = status?.message || 'outage';
   const hostname = status?.hostname || 'n/a';
-  const time = status?.time || 'n/a';
+  const time = status?.time || null;
 
   return (
     <tr className={success ? 'api-up' : 'api-outage'}>
       <td className="api-name">{api}</td>
-      <td>{time}</td>
+      <td>{time ? moment(time).format('LTS') : '----'}</td>
       <td>{hostname}</td>
       <td>{success ? message : <strong>{message}</strong>}</td>
     </tr>
